585bc5271075f33b3be2da3c99cec9d4f6a49aa9
Timur Kapitonov <kapitonov.timur@gmail.com>
2019-11-29 18:55:47 +0300

    29.11 - 1

---
+++ mysite/blog/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import blog_posts
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
