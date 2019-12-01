9faa1bf7f844774cc09f85d53ceb71ffc9f93745
Timur Kapitonov <kapitonov.timur@gmail.com>
2019-12-01 10:47:24 +0300

    made a feature for recommendation and email sent

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'kapitonov.timur@gmail.com'
+EMAIL_HOST_PASSWORD = 'Hfleuf13'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
