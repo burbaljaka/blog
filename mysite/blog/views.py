7a1135d5391050544184d2a4e7ea7652e32a617c
Timur Kapitonov <kapitonov.timur@gmail.com>
2019-12-01 14:44:40 +0300

    created cutsom tags for latest posts and most commented ones

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -55,8 +55,8 @@ def post_detail(request, year, month, day, post):
 	return render(request, 'blog/post/detail.html', {'post':post,
 													'comments':comments,
 													'new_comment':new_comment,
-													'comment_form':comment_form},
-													'similar_posts':similar_posts)
+													'comment_form':comment_form,
+													'similar_posts':similar_posts})
 
 def post_share(request, post_id):
 	post = get_object_or_404(Post, id = post_id, status = 'published')
