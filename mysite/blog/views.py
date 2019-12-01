e2fa0b443caa86f9570d42ab3f6b5e1565559093
Timur Kapitonov <kapitonov.timur@gmail.com>
2019-12-01 12:35:48 +0300

    added Tags system without possibility to add a tag to a post yet

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,26 +4,32 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 
-# def post_list(request):
-# 	posts = Post.published.all()
-# 	object_list = Post.published.all()
-# 	paginator = Paginator(object_list, 3)
-# 	page = request.GET.get('page')
-# 	try:
-# 		posts = paginator.page(page)
-# 	except PageNotAnInteger:
-# 		posts = paginator.page(1)
-# 	except EmptyPage:
-# 		posts = paginator.page(paginator.num_pages)
-#
-# 	return render (request, 'blog/post/list.html', {'posts':posts})
+def post_list(request, tag_slug = None):
+	object_list = Post.published.all()
+	tag = None
 
-class PostListView(ListView):
-	queryset = Post.published.all()
-	context_object_name = 'posts'
-	paginate_by = 3
-	template_name = 'blog/post/list.html'
+	if tag_slug:
+		tag = get_object_or_404(Tag, slug = tag_slug)
+		object_list = object_list.filter(tags__in = [tag])
+
+	paginator = Paginator(object_list, 3)
+	page = request.GET.get('page')
+	try:
+		posts = paginator.page(page)
+	except PageNotAnInteger:
+		posts = paginator.page(1)
+	except EmptyPage:
+		posts = paginator.page(paginator.num_pages)
+
+	return render (request, 'blog/post/list.html', {'posts':posts, 'tag':tag})
+
+# class PostListView(ListView):
+# 	queryset = Post.published.all()
+# 	context_object_name = 'posts'
+# 	paginate_by = 3
+# 	template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
 	post = get_object_or_404(Post, slug=post,
